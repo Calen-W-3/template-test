cc7c9a6a866eac0ffdb271bc3b0b7b2296ea218b
Calen <calenwill3@gmail.com>
2023-10-17 01:45:25 -0500

    Added changes to index, stle, and script files. Provided structure for the quote, author, and buttons.
    CSS file added a background image and provided a placeholder
    for the quote.
    
    Script file is a work in progress for random generation of quotes
    from the array or quotes.

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,1 +1,25 @@
-console.log("Hello!")
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+  //Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  // Only use constant if the value never changes, such as the URL will never change 
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// On Load
+getQuotes();
+
