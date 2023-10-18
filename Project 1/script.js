2fee9fd35e3e5bfdbc7d91ecb76d41b808157aec
Calen <calenwill3@gmail.com>
2023-10-18 15:49:01 -0500

    final draft od project

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,10 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
 let apiQuotes = [];
 
 //Show New Quote
 function newQuote() {
   //Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  //Chech if author field is black and replace it with 'unknown'
+  if(!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  //Populate text content of author and quote elements
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -20,6 +41,21 @@ async function getQuotes() {
   }
 }
 
+// Tweet Quote
+function tweetQuote() {
+  //template strings use back ticks
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
 getQuotes();
 
+
+
+//IDEA FOR MODIFICATION OF THIS PROJECT!!!
+//INSTAGRAM CAPTION GENERATOR 
